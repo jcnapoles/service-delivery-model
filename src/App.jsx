d7ec1137959c885aa4d18da2ba5e1671fd2f0715
Julio Cesar Napoles Puente <juliocesarnapolespuente@gmail.com>
2024-07-04 09:29:49 +0200

    Fix content extending beyond navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-        <div className='relative z-0'>
-        <Navbar />
-        <Hero />
-          <StarsCanvas />
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>        
+          <Navbar />
+          <Hero />
         </div>
-          
-        </div> 
+
         <div className='relative z-0'>
           <Experience /> 
           <StarsCanvas />
@@ -24,7 +21,8 @@ const App = () => {
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
-        </div>              
+        </div>
+        <StarsCanvas />               
       </div>
     </BrowserRouter>
   );
